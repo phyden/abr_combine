a2b3b571b5f509c29c77bb89134359e305c4c4d6
phyden <patrick_hyden@gmx.at>
2021-09-29 08:07:35 +0200

    FIX: imports

--- combine_view_with_style.py
+++ combine_view_with_style.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import numpy as np
 from styleframe import StyleFrame, Styler
 import os
 import sys
+import argparse
 
 parser = argparse.ArgumentParser(description="Combine output of multiple tools")
 
